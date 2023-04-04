0055df7ecd3e7deb08ea4afef3231b9054bf3d89
Roberto Ramirez <robert.624@hotmail.com>
2023-04-04 16:05:19 -0500

    added README

--- README.md
+++ README.md
@@ -1,35 +1,134 @@
-# age-calculator-app
+# Frontend Mentor - Age calculator app solution
 
-This template should help get you started developing with Vue 3 in Vite.
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-## Recommended IDE Setup
+## Table of contents
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
 
-## Customize configuration
+## Overview
 
-See [Vite Configuration Reference](https://vitejs.dev/config/).
+### The challenge
 
-## Project Setup
+Users should be able to:
 
-```sh
-npm install
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Solution URL](https://github.com/Robertron624/age-calculator-app)
+- Live Site URL: [Live site URL](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [Vue](https://vuejs.org/) - JS library
+- [Vite](https://vitejs.dev/) - Build tool
+
+### What I learned
+
+This was my first ever vue project and I learned a lot. I learned how to use vue and how to create a form in vue, how to bind the inputs
+to the data and how to handle the form. I also learned how to use the vue cli, conditional rendering and conditional classes to change
+the input colors to red when there are errors with the submitted date.
+
+Some code I am proud of:
+
+Each input with his label and error message.
+```html
+<div class="input-container">
+  <label :class="{'error-label': inputErrors.daysErrors || dateError}" for="day">Day</label>
+  <input
+    :class="{'error-border': inputErrors.daysErrors || dateError}"
+    v-model="daysInput"
+    placeholder="DD"
+    type="number"
+    name="day"
+    id="day"
+  />
+  <span v-if="inputErrors.daysErrors" class="error-msg">{{ inputErrors.daysErrors }}</span>
+</div>
 ```
 
-### Compile and Hot-Reload for Development
+The function that handles the form submission and validation.
+```js
+      calculate() {
 
-```sh
-npm run dev
+        if(this.checkEmpty() > 0) return;
+
+        const validDay = this.isNumberValid("day", this.daysInput);
+        const validMonth = this.isNumberValid("month", this.monthsInput);
+        const validYear = this.isNumberValid("year", this.yearsInput);
+
+        if (!validDay || !validMonth || !validYear) return;
+
+        if(!this.isDateValid(this.yearsInput, this.monthsInput, this.daysInput)) {
+          this.dateError = true;
+          return;
+        }else {
+          this.dateError = false;
+        }
+
+
+        const date = new Date(this.yearsInput, this.monthsInput, this.daysInput);
+        const today = new Date();
+
+        const diff = today - date;
+        const diffInYears = (diff / (1000 * 60 * 60 * 24)) / 365;
+        const diffInMonths =  Math.abs(today.getMonth() + date.getMonth());
+        const diffInDays = Math.abs(today.getUTCDate() - date.getUTCDate());
+        this.years = Math.floor(diffInYears);
+        this.months = Math.floor(diffInMonths);
+        this.days = 31 - Math.floor(diffInDays);
+        console.log("submitted !!")
+      },
 ```
 
-### Compile and Minify for Production
+### Continued development
 
-```sh
-npm run build
-```
+I want to keep using vue and learn more about it. I want to learn how to use vue router and pinia for managing global state. My main
+framework will still be react for intermediate and advance projects but I want to begin using vue for newbie and junior projects.
 
-### Lint with [ESLint](https://eslint.org/)
+### Useful resources
 
-```sh
-npm run lint
-```
+- [How to create a form in Vue.js](https://www.educative.io/answers/how-to-create-a-form-in-vuejs) - This helped me to create a form in vue and how to handle it.
+- [Get Year, Month and Day from a Date Object in JavaScript](https://bobbyhadz.com/blog/javascript-get-year-month-day-from-date) - This is an amazing article which helped me understand cuando to get the year, month and day from a date object in javascript to validate.
+- [Class and Style binding](https://vuejs.org/guide/essentials/class-and-style.html) how to use conditional classes in vue.
+- [Getting started with Vue](https://vuejs.org/guide/introduction.html)
+
+## Author
+
+- Personal Website - [Robert Ramirez](https://robert-ramirez.netlify.app)
+- Frontend Mentor User- [@Robertron624](https://www.frontendmentor.io/profile/Robertron624)
+- Twitter - [@robertdowny](https://www.twitter.com/robertdowny)
+
+## Acknowledgments
+
+- [Borislav Hadzhiev](https://bobbyhadz.com/) his article I mentioned in useful resources.
--- src/App.vue
+++ src/App.vue
@@ -1,159 +1,189 @@
 <script>
-  export default {
-    data() {
-      return {
-        years: "--",
-        months: "--",
-        days: "--",
-        yearsInput: "",
-        monthsInput: "",
-        daysInput: "",
-        inputErrors : {
-          yearsErrors: "",
-          monthsErrors: "",
-          daysErrors: "",
-        },
-        dateError: false,
+export default {
+  data() {
+    return {
+      years: '--',
+      months: '--',
+      days: '--',
+      yearsInput: '',
+      monthsInput: '',
+      daysInput: '',
+      inputErrors: {
+        yearsErrors: '',
+        monthsErrors: '',
+        daysErrors: ''
+      },
+      dateError: false
+    }
+  },
+  methods: {
+    checkEmpty() {
+      let empties = 0
+
+      const EMPTYMSG = 'This field is required'
+
+      if (this.yearsInput === '') {
+        this.inputErrors.yearsErrors = EMPTYMSG
+        empties++
+      } else {
+        this.inputErrors.yearsErrors = ''
+      }
+      if (this.monthsInput === '') {
+        this.inputErrors.monthsErrors = EMPTYMSG
+        empties++
+      } else {
+        this.inputErrors.monthsErrors = ''
+      }
+      if (this.daysInput === '') {
+        this.inputErrors.daysErrors = EMPTYMSG
+        empties++
+      } else {
+        this.inputErrors.daysErrors = ''
+      }
+
+      return empties
+    },
+
+    isNumberValid(type, value) {
+      const INVALIDMSG = 'Must be a valid '
+      const currentYear = new Date().getFullYear()
+      let isValid = true
+      switch (type) {
+        case 'day':
+          if (value > 31 || value < 1) {
+            isValid = false
+            this.inputErrors.daysErrors = INVALIDMSG + type
+          } else {
+            this.inputErrors.daysErrors = ''
+          }
+          break
+        case 'month':
+          if (value > 12 || value < 1) {
+            this.inputErrors.monthsErrors = INVALIDMSG + type
+            isValid = false
+          } else {
+            this.inputErrors.monthsErrors = ''
+          }
+          break
+        case 'year':
+          if (value > currentYear) {
+            this.inputErrors.yearsErrors = INVALIDMSG + type
+            isValid = false
+          } else {
+            this.inputErrors.yearsErrors = ''
+          }
+          break
+      }
+      return isValid
+    },
+
+    isDateValid(year, month, day) {
+      const date = new Date(year, month - 1, day) // month is 0 based, thats why we need to subtract 1 from the users input
+
+      // check if year, month and day are the same ( we stract 1 from the users input because in date object a month is 0 based )
+      if (date.getFullYear() == year && date.getMonth() == month - 1 && date.getDate() == day) {
+        return true
+      } else {
+        return false
       }
     },
-    methods: {
 
-      checkEmpty() {
+    calculate() {
+      if (this.checkEmpty() > 0) return
 
-        let empties = 0;
+      const validDay = this.isNumberValid('day', this.daysInput)
+      const validMonth = this.isNumberValid('month', this.monthsInput)
+      const validYear = this.isNumberValid('year', this.yearsInput)
 
-        const EMPTYMSG = "This field is required";
+      if (!validDay || !validMonth || !validYear) return
 
-        if (this.yearsInput === "") {
-          this.inputErrors.yearsErrors = EMPTYMSG;
-          empties++;
-        } else {
-          this.inputErrors.yearsErrors = "";
-        }
-        if (this.monthsInput === "") {
-          this.inputErrors.monthsErrors =  EMPTYMSG;
-          empties++;
-        } else {
-          this.inputErrors.monthsErrors = "";
-        }
-        if (this.daysInput === "") {
-          this.inputErrors.daysErrors =  EMPTYMSG;
-          empties++;
-        } else {
-          this.inputErrors.daysErrors = "";
-        }
+      if (!this.isDateValid(this.yearsInput, this.monthsInput, this.daysInput)) {
+        this.dateError = true
+        return
+      } else {
+        this.dateError = false
+      }
 
-        return empties
-      },
+      const date = new Date(this.yearsInput, this.monthsInput, this.daysInput)
+      const today = new Date()
 
-      isNumberValid (type, value) {
-        const INVALIDMSG = "Must be a valid ";
-        const currentYear = new Date().getFullYear();
-        let isValid = true;
-        switch(type) {
-          case "day":
-            if (value > 31 || value < 1) {
-              console.log("if day")
-              isValid = false;
-              this.inputErrors.daysErrors = INVALIDMSG + type;
-            } else {
-              this.inputErrors.daysErrors = "";
-            }
-            break;
-          case "month":
-            if (value > 12 || value < 1) {
-              console.log("if month")
-              this.inputErrors.monthsErrors = INVALIDMSG + type;
-              isValid = false;
-            } else {
-              this.inputErrors.monthsErrors = "";
-            }
-            break;
-          case "year":
-            if (value > currentYear) {
-              console.log("if year")
-              this.inputErrors.yearsErrors = INVALIDMSG + type;
-              isValid = false;
-            } else {
-              this.inputErrors.yearsErrors = "";
-            }
-            break;
-        }
-        return isValid;
-      },
-      
-      isDateValid (year, month, day) {
-        const date = new Date(year, month-1, day); // month is 0 based, thats why we need to subtract 1 from the users input
-
-        // check if year, month and day are the same ( we stract 1 from the users input because in date object a month is 0 based )
-        if (date.getFullYear() == year && date.getMonth() == month - 1 && date.getDate() == day) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-      
-      calculate() {
-
-        if(this.checkEmpty() > 0) return;
-
-        const validDay = this.isNumberValid("day", this.daysInput);
-        const validMonth = this.isNumberValid("month", this.monthsInput);
-        const validYear = this.isNumberValid("year", this.yearsInput);
-
-        if (!validDay || !validMonth || !validYear) return;
-
-        if(!this.isDateValid(this.yearsInput, this.monthsInput, this.daysInput)) {
-          this.dateError = true;
-          return;
-        }else {
-          this.dateError = false;
-        }
-
-
-        const date = new Date(this.yearsInput, this.monthsInput, this.daysInput);
-        const today = new Date();
-
-        const diff = today - date;
-        const diffInYears = (diff / (1000 * 60 * 60 * 24)) / 365;
-        const diffInMonths =  Math.abs(today.getMonth() + date.getMonth());
-        const diffInDays = Math.abs(today.getUTCDate() - date.getUTCDate());
-        this.years = Math.floor(diffInYears);
-        this.months = Math.floor(diffInMonths);
-        this.days = 31 - Math.floor(diffInDays);
-        console.log("submitted !!")
-      },
-    },
+      const diff = today - date
+      const diffInYears = diff / (1000 * 60 * 60 * 24) / 365
+      const diffInMonths = Math.abs(today.getMonth() + date.getMonth())
+      const diffInDays = Math.abs(today.getUTCDate() - date.getUTCDate())
+      this.years = Math.floor(diffInYears)
+      this.months = Math.floor(diffInMonths)
+      this.days = 31 - Math.floor(diffInDays)
+      console.log('submitted !!')
+    }
   }
-
+}
 </script>
 
 <template>
-
   <main>
     <div class="container">
       <div class="date-inputs">
         <form action="GET" @submit.prevent="calculate">
           <div class="inputs">
             <div class="input-container">
-                <label :class="{'error-label': inputErrors.daysErrors || dateError}" for="day">Day</label>
-                <input :class="{'error-border': inputErrors.daysErrors || dateError}" v-model="daysInput" placeholder="DD" type="number" name="day" id="day">
-                <span v-if="inputErrors.daysErrors" class="error-msg">{{ inputErrors.daysErrors }}</span>
+              <label :class="{ 'error-label': inputErrors.daysErrors || dateError }" for="day"
+                >Day</label
+              >
+              <input
+                :class="{ 'error-border': inputErrors.daysErrors || dateError }"
+                v-model="daysInput"
+                placeholder="DD"
+                type="number"
+                name="day"
+                id="day"
+              />
+              <span v-if="inputErrors.daysErrors" class="error-msg">{{
+                inputErrors.daysErrors
+              }}</span>
             </div>
             <div class="input-container">
-                <label :class="{'error-label': inputErrors.monthsErrors || dateError}" for="month">Month</label>
-                <input :class="{'error-border': inputErrors.monthsErrors || dateError}" v-model="monthsInput" placeholder="MM" type="number" name="month" id="month">
-                <span v-if="inputErrors.monthsErrors" class="error-msg">{{ inputErrors.monthsErrors }}</span>
+              <label :class="{ 'error-label': inputErrors.monthsErrors || dateError }" for="month"
+                >Month</label
+              >
+              <input
+                :class="{ 'error-border': inputErrors.monthsErrors || dateError }"
+                v-model="monthsInput"
+                placeholder="MM"
+                type="number"
+                name="month"
+                id="month"
+              />
+              <span v-if="inputErrors.monthsErrors" class="error-msg">{{
+                inputErrors.monthsErrors
+              }}</span>
             </div>
             <div class="input-container">
-              <label :class="{'error-label': inputErrors.yearsErrors || dateError}" for="year">year</label>
-              <input :class="{'error-border': inputErrors.yearsErrors || dateError}" v-model="yearsInput" placeholder="YYYY" type="number" name="year" id="year">
-              <span v-if="inputErrors.yearsErrors" class="error-msg">{{ inputErrors.yearsErrors }}</span>
+              <label :class="{ 'error-label': inputErrors.yearsErrors || dateError }" for="year"
+                >year</label
+              >
+              <input
+                :class="{ 'error-border': inputErrors.yearsErrors || dateError }"
+                v-model="yearsInput"
+                placeholder="YYYY"
+                type="number"
+                name="year"
+                id="year"
+              />
+              <span v-if="inputErrors.yearsErrors" class="error-msg">{{
+                inputErrors.yearsErrors
+              }}</span>
             </div>
           </div>
           <div class="button-container">
             <button role="button" class="arrow" type="submit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44">
+                <g fill="none" stroke="#FFF" stroke-width="2">
+                  <path
+                    d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"
+                  />
+                </g>
+              </svg>
             </button>
           </div>
         </form>
@@ -170,14 +200,12 @@
 </template>
 
 <style scoped>
-
 main {
   --off-white: hsl(0, 0%, 94%);
   --light-grey: hsl(0, 0%, 86%);
   --smokey-grey: hsl(0, 1%, 44%);
   --purple: hsl(259, 100%, 65%);
   --light-red: hsl(0, 100%, 67%);
-
 }
 
 .container {
@@ -208,14 +236,14 @@ main {
   width: 58px;
   background-color: var(--purple);
   border-radius: 50%;
-  padding: .8rem;
-  transition: all .3s ease-in-out;
+  padding: 0.8rem;
+  transition: all 0.3s ease-in-out;
   cursor: pointer;
   align-self: center;
 }
 
 .arrow::after {
-  content: "";
+  content: '';
   height: 2px;
   width: 100%;
   background-color: var(--purple);
@@ -231,10 +259,10 @@ main {
   background-color: black;
 }
 
-.date-inputs form{
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
+.date-inputs form {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
 }
 
 .date-inputs .inputs {
@@ -262,7 +290,7 @@ main {
   border: 1px solid var(--light-grey);
   outline: none;
   border-radius: 5px;
-  padding-left: .5rem;
+  padding-left: 0.5rem;
 }
 
 .date-inputs input:focus {
@@ -293,7 +321,7 @@ main {
 .error-msg {
   color: var(--light-red);
   font-size: 0.5rem;
-  margin-top: .3rem;
+  margin-top: 0.3rem;
   font-weight: 300;
   text-transform: capitalize;
   font-style: italic;
@@ -332,6 +360,5 @@ main {
   .calculation {
     padding-top: 2rem;
   }
-
 }
 </style>
