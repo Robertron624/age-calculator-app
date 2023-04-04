e2dbd725fd9688c2a5af52c44184345631c610d7
Roberto Ramirez <robert.624@hotmail.com>
2023-04-04 15:24:01 -0500

    fix: button position now is responsive

--- src/App.vue
+++ src/App.vue
@@ -134,24 +134,28 @@
     <div class="container">
       <div class="date-inputs">
         <form action="GET" @submit.prevent="calculate">
-          <div class="input-container">
-              <label :class="{'error-label': inputErrors.daysErrors || dateError}" for="day">Day</label>
-              <input :class="{'error-border': inputErrors.daysErrors || dateError}" v-model="daysInput" placeholder="DD" type="number" name="day" id="day">
-              <span v-if="inputErrors.daysErrors" class="error-msg">{{ inputErrors.daysErrors }}</span>
+          <div class="inputs">
+            <div class="input-container">
+                <label :class="{'error-label': inputErrors.daysErrors || dateError}" for="day">Day</label>
+                <input :class="{'error-border': inputErrors.daysErrors || dateError}" v-model="daysInput" placeholder="DD" type="number" name="day" id="day">
+                <span v-if="inputErrors.daysErrors" class="error-msg">{{ inputErrors.daysErrors }}</span>
+            </div>
+            <div class="input-container">
+                <label :class="{'error-label': inputErrors.monthsErrors || dateError}" for="month">Month</label>
+                <input :class="{'error-border': inputErrors.monthsErrors || dateError}" v-model="monthsInput" placeholder="MM" type="number" name="month" id="month">
+                <span v-if="inputErrors.monthsErrors" class="error-msg">{{ inputErrors.monthsErrors }}</span>
+            </div>
+            <div class="input-container">
+              <label :class="{'error-label': inputErrors.yearsErrors || dateError}" for="year">year</label>
+              <input :class="{'error-border': inputErrors.yearsErrors || dateError}" v-model="yearsInput" placeholder="YYYY" type="number" name="year" id="year">
+              <span v-if="inputErrors.yearsErrors" class="error-msg">{{ inputErrors.yearsErrors }}</span>
+            </div>
           </div>
-          <div class="input-container">
-              <label :class="{'error-label': inputErrors.monthsErrors || dateError}" for="month">Month</label>
-              <input :class="{'error-border': inputErrors.monthsErrors || dateError}" v-model="monthsInput" placeholder="MM" type="number" name="month" id="month">
-              <span v-if="inputErrors.monthsErrors" class="error-msg">{{ inputErrors.monthsErrors }}</span>
+          <div class="button-container">
+            <button role="button" class="arrow" type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
+            </button>
           </div>
-          <div class="input-container">
-            <label :class="{'error-label': inputErrors.yearsErrors || dateError}" for="year">year</label>
-            <input :class="{'error-border': inputErrors.yearsErrors || dateError}" v-model="yearsInput" placeholder="YYYY" type="number" name="year" id="year">
-            <span v-if="inputErrors.yearsErrors" class="error-msg">{{ inputErrors.yearsErrors }}</span>
-          </div>
-          <button role="button" class="arrow" type="submit">
-          <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
-        </button>
         </form>
         <!-- <img class="arrow" src="icon-arrow.svg" alt="arrow"> -->
         <span v-if="dateError" class="error-msg">Must be a valid date</span>
@@ -184,29 +188,42 @@ main {
   border-radius: 20px 20px 26% 20px;
 }
 
-
 .date-inputs {
-  padding: 2rem 0 3rem 0;
-  border-bottom: 1px solid var(--light-grey);
+  padding: 2rem 0 0rem 0;
+  /* border-bottom: 1px solid var(--light-grey); */
   position: relative;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 3.5rem;
+  position: relative;
+  border-bottom: 1px solid var(--light-grey);
 }
 
 .arrow {
   border-color: transparent;
   position: absolute;
-  bottom: -175%;
-  right: 39%;
-  transform: translateY(-50%);
+  width: 58px;
   background-color: var(--purple);
   border-radius: 50%;
   padding: .8rem;
   transition: all .3s ease-in-out;
   cursor: pointer;
+  align-self: center;
+}
+
+.arrow::after {
+  content: "";
+  height: 2px;
+  width: 100%;
+  background-color: var(--purple);
 }
 
 .arrow svg {
   display: block;
-  width: 25px;
+  width: 100%;
   height: 25px;
 }
 
@@ -214,10 +231,15 @@ main {
   background-color: black;
 }
 
-
 .date-inputs form{
     display: flex;
     justify-content: space-between;
+    flex-direction: column;
+}
+
+.date-inputs .inputs {
+  display: flex;
+  justify-content: space-between;
 }
 
 .date-inputs .input-container {
@@ -233,7 +255,7 @@ main {
 }
 
 .date-inputs input {
-  width: 4rem;
+  width: 5rem;
   height: 2.4rem;
   font-size: 1rem;
   font-weight: 600;
@@ -270,7 +292,8 @@ main {
 
 .error-msg {
   color: var(--light-red);
-  font-size: 0.6rem;
+  font-size: 0.5rem;
+  margin-top: .3rem;
   font-weight: 300;
   text-transform: capitalize;
   font-style: italic;
@@ -291,15 +314,24 @@ main {
   }
 
   .arrow {
-    right: -28%;
-  }
-
-  .date-inputs input {
-    width: 5rem;
+    right: 0%;
   }
 
   .date-inputs form {
+    width: 100%;
+  }
+
+  .date-inputs .inputs {
     width: 80%;
   }
+
+  .button-container {
+    margin-top: 1.5rem;
+  }
+
+  .calculation {
+    padding-top: 2rem;
+  }
+
 }
 </style>
